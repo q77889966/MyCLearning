8ec7e850ef085a36b4256fe4bfbc08e9d381ebf7
q77889966 <2304144391@qq.com>
2016-08-02 14:26:30 +0800

    Added WriteTextFile.cpp

---
+++ WriteTextFile.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	FILE *f = fopen("file.txt", "w");
+	if (f == NULL)
+	{
+		printf("Error opening file!\n");
+		exit(1);
+	}
+	
+	const char *text = "≤‚ ‘Œƒ±æ";
+	fprintf(f, "Some text: %s\n", text);
+	
+	int i = 1;
+	float py = 3.1415927;
+	fprintf(f, "Integer: %d, float: %f\n", i, py);
+	
+	char c = 'A';
+	fprintf(f, "A character: %c\n", c);
+	
+	fclose(f);
+	return 0;
+}
