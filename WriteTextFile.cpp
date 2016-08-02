d67fc7618de89b77d6d1c4526701794ab5dc8d22
q77889966 <2304144391@qq.com>
2016-08-02 15:18:24 +0800

    Update WriteTextFile.cpp

--- WriteTextFile.cpp
+++ WriteTextFile.cpp
@@ -5,7 +5,7 @@ int main()
 	FILE *f = fopen("file.txt", "w");
 	if (f == NULL)
 	{
-		printf("Error opening file!\n");
+		printf("\n");
 		exit(1);
 	}
 	
@@ -14,10 +14,10 @@ int main()
 	
 	int i = 1;
 	float py = 3.1415927;
-	fprintf(f, "Integer: %d, float: %f\n", i, py);
+	fprintf(f, " %d %f\n", i, py);
 	
 	char c = 'A';
-	fprintf(f, "A character: %c\n", c);
+	fprintf(f, "%c\n", c);
 	
 	fclose(f);
 	return 0;
